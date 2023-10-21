ae71cb524f01ef14057eb2d686fc5709aada9e24
qkrckstjq <117289923+qkrckstjq@users.noreply.github.com>
2023-10-21 18:21:48 +0900

    자료형

--- src/sec02/chap03/Ex01.java
+++ src/sec02/chap03/Ex01.java
@@ -1,2 +1,16 @@
-package sec02.chap03;public class Ex01 {
+package sec02.chap03;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        boolean live = true;
+        double pi = 3.14;
+        int age = 23;
+        final double gravity = 9.81;
+        char sex = 'M';
+        String name = "박찬섭";
+
+
+        System.out.println("pi = " + pi);
+        System.out.println(name);
+    }
 }
